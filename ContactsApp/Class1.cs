969663f4726c459b9d57292d1c3b083cd67853a3
Михаил Сидоренко <msidorenko600@gmail.com>
2024-05-11 21:39:01 +0700

    Добавлен проект логики

--- ContactsApp/Class1.cs
+++ ContactsApp/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace ContactsApp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
 {
     public class Class1
     {
-
     }
 }
