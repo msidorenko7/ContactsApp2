ed1adbcdf7b566cfbd44b88fdbd07f72f29776ba
msidorenko7 <msidorenko600@gmail.com>
2024-06-08 19:43:51 +0700

    Добавлен код Юнит-Тестов.

--- ContactsApp.UnitTests/ContactTest.cs
+++ ContactsApp.UnitTests/ContactTest.cs
@@ -3,10 +3,202 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using ContactsApp;
 
-namespace ContactsApp.UnitTests
+namespace ContactsApp.Model.UnitTests
 {
-    internal class ContactTest
+    [TestFixture]
+    public class ContactTests
     {
+        private Contact _contact;
+
+        [SetUp]
+        public void InitContact()
+        {
+            _contact = new Contact();
+        }
+
+        /// <summary>
+        /// Позитивный тест геттера FullName
+        /// </summary>
+        [Test(Description = "Позитивный тест геттера FullName")]
+        public void TestFullNameGet_CorrectValue()
+        {
+            // Arrange
+            var expected = "Смирнов Андрей";
+            _contact.FullName = expected;
+
+            // Act
+            var actual = _contact.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер FullName возвращает неправильную фамилию");
+        }
+
+        /// <summary>
+        /// Негативный тест для проверки сеттера имени и фамилии
+        /// </summary>
+        /// <param name="wrongFullName">переменная FullName для теста</param>
+        /// <param name="message">Сообщение при ошибки теста</param>
+        [TestCase("", "Должно возникать исключение, если фамилия и имя - пустая строка",
+        TestName = "Присвоение пустой строки в качестве фамилии и имени")]
+        [TestCase("Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-СмирновСмирнов-Смирнов-Смирнов-Смирнов-Смирнов-СмирновСмирнов-Смирнов-Смирнов-Смирнов-Смирнов-СмирновСмирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов",
+        "Должно возникать исключение, если фамилия и имя длиннее 100 символов",
+        TestName = "Присвоение неправильной фамилии и имени больше 100 символов")]
+        public void TestSurnameSet_ArgumentException(string wrongFullName, string
+        message)
+        {
+            Assert.Throws<ArgumentException>(
+            () => { _contact.FullName = wrongFullName; },
+            message);
+        }
+
+        /// <summary>
+        /// Позитивный тест геттера PhoneNumber
+        /// </summary>
+        [Test(Description = "Позитивный тест геттера PhoneNumber")]
+        public void TestPhoneNumberGet_CorrectValue()
+        {
+            // Arrange
+            var expected = "9043769910";
+            _contact.PhoneNumber = expected;
+
+            // Act
+            var actual = _contact.PhoneNumber;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер PhoneNumber возвращает неправильный номер телефона");
+        }
+
+        /// <summary>
+        /// Негативный тест для проверки сеттера телефонного номера
+        /// </summary>
+        /// <param name="wrongPhoneNumber">переменная PhoneNumber для теста</param>
+        /// <param name="message">Сообщение при ошибки теста</param>
+        [TestCase("", "Должно возникать исключение, если телефон-пустая строка",
+       TestName = "Присвоение пустой строки в качестве телефонного номера")]
+        [TestCase("90430769910",
+       "Должно возникать исключение, если телефон введен неверно(больше)",
+       TestName = "Присвоение телефонного номера больше 10 символов")]
+        [TestCase("769910",
+       "Должно возникать исключение, если телефон введен неверно(меньше)",
+       TestName = "Присвоение телефонного номера меньше 10 символов")]
+        public void TestPhoneNumberSet_ArgumentException(string wrongPhoneNumber, string
+       message)
+        {
+            Assert.Throws<ArgumentException>(
+            () => { _contact.PhoneNumber = wrongPhoneNumber; },
+            message);
+        }
+
+        /// <summary>
+        /// Позитивный тест геттера Vk
+        /// </summary>
+        [Test(Description = "Позитивный тест геттера Vk")]
+        public void TestVkGet_CorrectValue()
+        {
+            // Arrange
+            var expected = "allloooo";
+            _contact.Vk = expected;
+
+            // Act
+            var actual = _contact.Vk;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер Vk возвращает неправильный Id vk");
+        }
+
+        /// <summary>
+        /// Негативный тест для проверки сеттера vk
+        /// </summary>
+        /// <param name="wrongVk">переменная Vk для теста</param>
+        /// <param name="message">Сообщение при ошибки теста</param>
+        [TestCase("", "Должно возникать исключение, если id vk-пустая строка",
+      TestName = "Присвоение пустой строки в качестве Id vk номера")]
+        [TestCase("90430769910904307699109043076991090430769910904307699109043076991090430769910904307699109043076991090430769910",
+      "Должно возникать исключение, если id vk введен неверно",
+      TestName = "Присвоение id vk больше 50 символов")]
+        public void TestVkSet_ArgumentException(string wrongVk, string
+      message)
+        {
+            Assert.Throws<ArgumentException>(
+            () => { _contact.Vk = wrongVk; },
+            message);
+        }
+
+        /// <summary>
+        /// Позитивный тест геттера Email
+        /// </summary>
+        [Test(Description = "Позитивный тест геттера Email")]
+        public void TestEmailGet_CorrectValue()
+        {
+            // Arrange
+            var expected = "alll@oooo";
+            _contact.Email = expected;
+
+            // Act
+            var actual = _contact.Email;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер Email возвращает неправильный Email");
+        }
+
+        /// <summary>
+        /// Негативный тест для Email
+        /// </summary>
+        /// <param name="wrongEmail">Переменная Email для теста</param>
+        /// <param name="message">Сообщение при ошибки теста</param>
+        [TestCase("", "Должно возникать исключение, если Email -пустая строка",
+      TestName = "Присвоение пустой строки в качестве Email")]
+        [TestCase("dklfdkldfkfk",
+      "Должно возникать исключение, если Email введен без @",
+      TestName = "Присвоение Email без символа @")]
+        [TestCase("ллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвалллвдладвал",
+      "Долж возникать исключение, если Email больше 100 символов",
+      TestName = "Присвоение Email более 100 символов")]
+        public void TestEmailSet_ArgumentException(string wrongEmail, string
+      message)
+        {
+            Assert.Throws<ArgumentException>(
+            () => { _contact.Email = wrongEmail; },
+            message);
+        }
+
+        /// <summary>
+        /// Позитивный тест геттера DateOfBirth
+        /// </summary>
+        [Test(Description = "Позитивный тест геттера DateOfBirth")]
+        public void TestDateOfBirthGet_CorrectValue()
+        {
+            // Arrange
+            var expected = new DateTime(1999, 02, 06);
+            _contact.DateOfBirth = expected;
+
+            // Act
+            var actual = _contact.DateOfBirth;
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Геттер DateOfBirth возвращает неправильную дату рождения");
+        }
+
+        /// <summary>
+        /// негативный тест для даты рождения
+        /// </summary>
+        /// <param name="wrongDate">Переменна даты рождения для теста </param>
+        /// <param name="message">Сообщение при ошибки теста</param>
+        [TestCase("1700,02,04",
+      "Должно возникать исключение, если дата рождения меньше минимальной",
+      TestName = "Присвоение DateOfBirth неправильную дату(Меньше минимальной)")]
+        [TestCase("2027,02,02",
+      "Долж возникать исключение, если дата рождения больше сегодняшнего дня",
+      TestName = "Присвоение DateOfBirth позднее сегодняшнего дня")]
+        public void TestDateOfBirthSet_ArgumentException(DateTime wrongDate, string
+      message)
+        {
+            Assert.Throws<ArgumentException>(
+            () => { _contact.DateOfBirth = wrongDate; },
+            message);
+        }
     }
 }
--- ContactsApp.UnitTests/ProjectTest.cs
+++ ContactsApp.UnitTests/ProjectTest.cs
@@ -3,10 +3,206 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using ContactsApp;
 
-namespace ContactsApp.UnitTests
+namespace ContactsApp.Model.UnitTests
 {
-    internal class ProjectTest
+    [TestFixture]
+    public class ProjectTests
     {
+        [Test(Description = "Позитивный тест по передачи правильного списка")]
+        public void TestProject_CorrectValue()
+        {
+            // Arrange
+            var testProject = InitProject();
+
+            // Act
+            var expectedList = testProject.Contacts;
+
+            // Assert
+            Assert.AreEqual(expectedList, testProject.Contacts,
+            "Был передан неправильный список");
+        }
+
+        [Test(Description = "Позитивный тест сортировки c отсартирвоанными контактами")]
+        public void TestSortContactsByFullName()
+        {
+            // Arrange
+            var testProject = InitProjectSort();
+
+            // Act
+            IEnumerable<string> expected = new[]
+            {
+                "FullName1", "FullName2", "FullName3",
+            };
+            List<Contact> sortedContacts = testProject.SortContactsByFullName();
+
+            // Assert
+            Assert.IsTrue(sortedContacts.Select(n => n.FullName).SequenceEqual(expected),
+            "Список отсортирован неверно");
+        }
+
+
+        [Test(Description = "Позитивный тест сортировки c несортированными контактами")]
+        public void TestSortContactsByFullName_1()
+        {
+            // Arrange
+            var testProject = InitProject();
+
+            // Act
+            IEnumerable<string> expected = new[]
+            {
+                "FullName1", "FullName2", "FullName3",
+            };
+            List<Contact> sortedContacts = testProject.SortContactsByFullName();
+
+            // Assert
+            Assert.IsTrue(sortedContacts.Select(n => n.FullName).SequenceEqual(expected),
+            "Список отсортирован неверно");
+        }
+
+        /// <summary>
+        /// Позитивный поиск с заданной подстрокой и сущестующим списком
+        /// </summary>
+        /// <param name="substring"></param>
+        [TestCase("FullName1", TestName = "Тест сортировки с верной  подстрокой")]
+        public void TestFindContactsByFullName_CorrectValue(string substring)
+        {
+            // Arrange
+            var testProject = InitProject();
+
+            // Act
+            IEnumerable<string> expected = new[]
+            {
+                "FullName1",
+            };
+            List<Contact> sortedContacts = testProject.FindByFullName(substring);
+
+            // Assert
+            Assert.IsTrue(sortedContacts.Select(n => n.FullName).SequenceEqual(expected),
+                     "Список отсортирован неверно");
+        }
+
+        [TestCase("FullName1", TestName = "Тест сортировки с задананной подстракой и пустым списком")]
+        public void TestFindContactsByFullName_EmptyList(string substring)
+        {
+            // Arrange
+            var testProject = new Project();
+
+            // Act
+            List<Contact> sortedContacts = testProject.FindByFullName(substring);
+
+            // Assert
+            Assert.IsEmpty(sortedContacts,
+                     "Список отсортирован неверно");
+        }
+
+        [TestCase("", TestName = "Тест сортировки с пустой подстрокой")]
+        public void TestFindContactsByFullName_EmptySubstring(string wrongSubstring)
+        {
+            // Arrange
+            var testProject = InitProjectSort();
+
+            // Act
+            IEnumerable<string> expected = new[]
+            {
+                "FullName1", "FullName2", "FullName3",
+            };
+            List<Contact> sortedContacts = testProject.FindByFullName(wrongSubstring);
+
+            // Assert
+            Assert.IsTrue(sortedContacts.Select(n => n.FullName).SequenceEqual(expected),
+            "Список отсортирован неверно");
+        }
+
+        [Test(Description = "Негативный тест сортировки")]
+        public void TestSortContactsByFullName_EmptyList()
+        {
+            // Arrange
+            var testProject = new Project();
+
+            // Act
+            List<Contact> sortedContacts = testProject.SortContactsByFullName();
+
+            // Assert
+            Assert.IsEmpty(sortedContacts, "Список осторирован по строке неверно");
+        }
+
+
+        /// <summary>
+        /// Негативный тест
+        /// </summary>
+        /// <param name="wrongSubstring"></param>
+        [TestCase("1testsFullName111", TestName = "Тест сортировки с подстрокой больше фамилии")]
+        public void TestFindContactsByFullName_WrongSubstring(string wrongSubstring)
+        {
+            // Arrange
+            var testProject = InitProject();
+
+            // Act
+            List<Contact> sortedContacts = testProject.FindByFullName(wrongSubstring);
+
+            // Assert
+            Assert.IsEmpty(sortedContacts, "Список осторирован по строке неверно");
+        }
+
+        static public Project InitProject()
+        {
+            var testProject = new Project();
+
+            testProject.Contacts.Add(new Contact(
+            "FullName2",
+            "Test@Email1",
+            "9043769910",
+            "TestiDVK1",
+            new DateTime(2000, 1, 1)
+            ));
+
+            testProject.Contacts.Add(new Contact(
+            "FullName1",
+            "Test@Email2",
+            "9043769911",
+            "TestiDVK",
+            new DateTime(2005, 05, 01)));
+
+            testProject.Contacts.Add(new Contact(
+            "FullName3",
+            "Test@Email3",
+            "9043799913",
+            "TestiDVK3",
+            new DateTime(2002, 03, 11)));
+
+            return testProject;
+        }
+
+        static public Project InitProjectSort()
+        {
+            var testProject = new Project();
+
+            testProject.Contacts.Add(new Contact(
+            "FullName1",
+            "Test@Email1",
+            "9043769910",
+            "TestiDVK1",
+            new DateTime(2000, 1, 1)
+            ));
+
+            testProject.Contacts.Add(new Contact(
+            "FullName2",
+            "Test@Email2",
+            "9043769911",
+            "TestiDVK",
+            new DateTime(2005, 05, 01)));
+
+            testProject.Contacts.Add(new Contact(
+            "FullName3",
+            "Test@Email3",
+            "9043799913",
+            "TestiDVK3",
+            new DateTime(2002, 03, 11)));
+
+            return testProject;
+        }
     }
 }
